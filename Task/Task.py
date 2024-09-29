76b1426b19b6c2e5a73e5cdcc4b1df719528f5b7
mohamedyasser <mohamedyasser2119@yahoo.com>
2024-09-29 16:54:53 +0200

    second commit

---
+++ Task/Task.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+file_path = '/mnt/data/Employees.csv'
+df = pd.read_csv(file_path)
+
+df = df.drop_duplicates()
+
+df['Age'] = df['Age'].apply(lambda x: int(x))
+
+
+usd_to_egp_rate = 49
+df['Salary(EGP)'] = df['Salary(USD)'] * usd_to_egp_rate
+
+
+average_age = df['Age'].mean()
+median_salary_egp = df['Salary(EGP)'].median()
+
+
+gender_counts = df['Gender'].value_counts()
+male_female_ratio = gender_counts.get('M', 0) / gender_counts.get('F', 1)  # Prevent division by zero
+
+
+print("Average Age:", average_age)
+print("Median Salary (EGP):", median_salary_egp)
+print("Male to Female Ratio:", male_female_ratio)
+
+
+output_file_path = '/mnt/data/Updated_Employees.csv'
+df.to_csv(output_file_path, index=False)
+
+
+output_file_path
